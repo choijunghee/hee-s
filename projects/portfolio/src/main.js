8a4690368f6915aae3bb13b34be3cc6144d29566
choijunghee <bcfkorea@hanmail.net>
2023-09-13 08:32:41 +0900

    Update main.js

--- projects/portfolio/src/main.js
+++ projects/portfolio/src/main.js
@@ -1,41 +1,16 @@
-'use strict';
-
-// header에 페이지 아래로 스크롤시 다크 스타일링 적용
+//Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header=document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
-    if(window.scrollY > headerHeight){
-        header.classList.add('header-dark');
-    }else{
-        header.classList.remove('header-dark');
-    }
+const headerHeight = header.offsetHeight;
+document.addEventListener('scroll', ()=>{
+if(window.scrollY>headerHeight){
+    header.classList.add('header-dark');
+}else{
+    header.classList.remove('header-dark');
+}
 });
-
-// home섹션을 아래로 스크롤시 투명하게 처리함
+//hom 색션을 아래로 스크롤시 투명하게 처리함
 const home = document.querySelector('.home_container');
-const homeHeight=home.offsetHeight;
+const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', ()=> {
-    home.style.opacity= 1-window.scrollY /homeHeight;
-});
-
-// Arrow up버튼을 아래로 스크롤시 투명하게 처리함
-const arrowUp = document.querySelector('.arrow-up');
-document.addEventListener('scroll', () =>{
-    if(window.scrollY> homeHeight/2){
-        arrowUp.style.opacity=1;
-    } else{
-        arrowUp.style.opacity=0;
-    }
-});
-
-// NavBar 토글버튼 클릭 처리
-const navbarMenu= document.querySelector('.header_menu');
-const navbarToggle = document.querySelector('.header_toggle');
-navbarToggle.addEventListener('click', ()=> {
-    navbarMenu.classList.toggle('open');
-});
- 
-// Navbar 메뉴 클릭시 메뉴를 자동으로 닫아줌
-navbarMenu.addEventListener('click', ()=> {
-    navbarMenu.classList.remove('open');
+    home. style.opacity=1-window.scrollY / homeHeight;
 });
